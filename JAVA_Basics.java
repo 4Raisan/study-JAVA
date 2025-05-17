2bede739f72d026c70405eaaecbe4a0f3c331e56
4Raisan <mn4raisan@gmail.com>
2025-05-17 18:59:51 +0530

    add JAVA rule 1

--- JAVA_Basics.java
+++ JAVA_Basics.java
@@ -8,11 +8,14 @@
 // Single-line comment  
 /*  Multi-line comment  */ 
 
-// public and private
-// private - Only this class itself can see & use this 
-//         - just for class 
-// public - Other classes also see & use this 
-//        - NEED object 
+/* Rules of JAVA:
+1. Only one public class per .java file, and the file name must match that public class name.
+
+ public and private
+ private - Only this class itself can see & use this 
+         - just for class 
+ public - Other classes also see & use this 
+        - NEED object  */
 
 // use this.[instance variables]  - (same as python self.)
 this.age = age;
