d1b085ec00d3f43a1b4c9afc99863b3e99f058cf
Rashen Anupama <mn4raisan@gmail.com>
2025-06-18 16:11:16 +0530

    reshape q1 q2

--- CSAnswers.java
+++ CSAnswers.java
@@ -1,17 +1,24 @@
 class CSAnswers {
-    // Marks enter
-    public CSAnswers(int num) {
-        this.num = num;
-    }
+
+    public static int num = 70;
 
     // check range
-    public String check() {
-        if (num>=0 & num<=100) {
-            System.out.println("Green");
-            return "Hi";
+    public static String check() {
+        if (num>=0 && num<=100) {
+            String x = "Green";
+            System.out.println(x+" LK");
+            return x;
+        } else {
+            return "Guy";
         }
     }
+
+
     public static void main (String[] args) {
 
+        System.out.println(check());
+        System.out.println();
+        
+        
     }
 }
