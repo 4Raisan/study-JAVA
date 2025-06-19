e1a3764b98c442805fb6d7b458e270fd56a723e2
Rashen Anupama <mn4raisan@gmail.com>
2025-06-19 16:31:29 +0530

    done q3

--- CSAnswers.java
+++ CSAnswers.java
@@ -1,8 +1,9 @@
 class CSAnswers {
 
+    // 1
     public static int num = 70;
 
-    // check range
+    // 2
     public static String check() {
         if (num>=0 && num<=100) {
             String x = "Green";
@@ -13,12 +14,21 @@ class CSAnswers {
         }
     }
 
+    // 3
+    public static void failed() {
+        if (num>=50) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+
 
     public static void main (String[] args) {
 
         System.out.println(check());
         System.out.println();
-        
-        
+        failed();
+
     }
 }
