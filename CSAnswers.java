23356f19397e28e8dbcd75bd382bd10a5b3162c8
Rashen Anupama <mn4raisan@gmail.com>
2025-06-19 17:03:05 +0530

    done q4

--- CSAnswers.java
+++ CSAnswers.java
@@ -23,7 +23,39 @@ class CSAnswers {
         }
     }
 
-
+    //4
+    public static void four() {
+        int cs = 4;
+        if (num>=90 && num<101) {
+            cs = 1;
+        } else if (num>=75) {
+            cs =2;
+        } else if (num>=60) {
+            cs = 3;
+        } else if (num>=50) {
+            cs = 4;
+        } else {
+            cs = 5;
+        }
+        switch (cs) {
+            case 1:
+                System.out.println("A");
+                break;
+            case 2:
+                System.out.println("B");
+                break;
+            case 3:
+                System.out.println("C");
+                break;
+            case 4:
+                System.out.println("D");
+                break;
+            default:
+                System.out.println("F");
+                break;
+        }
+    }
+    
     public static void main (String[] args) {
 
         System.out.println(check());
